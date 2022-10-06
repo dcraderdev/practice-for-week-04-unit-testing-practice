dfba2b95e1f579a2c68ae81ac61c34f28a2b4ca3
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-10-05 17:29:12 -0700

    complete
    complete

--- funcs.js
+++ funcs.js
@@ -1,14 +1,69 @@
+const { AssertionError } = require("chai")
+
 function isFive(num) {
-  // Your code here
+  if(num !== 5){
+  return false
+} else {
+  return true
+}
 }
 
-function isOdd(number) {
-  // Your code here
+
+
+function isOdd(num) {
+
+  if(typeof num !== 'number'){
+    throw new Error('Error')
+  }
+
+else {
+  if(num % 2 !== 0){
+    return true
+  } else {
+    return false
+  }
+
+
+  }
+
+  
 }
+
+
+
+// function isOdd(num) {
+// try{ 
+// if(typeof num === 'number'){
+
+//   if(num % 2 !== 0){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+//   else{
+//     throw new Error("Error")
+//   }
+// } catch (e) {
+//   console.log(e.message);
+// }
+// }
+
+
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let newArr = [];
+  if (min > max) {
+    return newArr
+  }
+  else {
+    for (let i = min; i <= max; i += step) {
+      newArr.push(i)
+    }
+    return newArr
+  }
 }
+
 
 
 module.exports = { isFive, isOdd, myRange };
